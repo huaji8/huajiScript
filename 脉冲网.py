9eb43d4faab1a8fcd37b74c1533f37038f64e4d6
滑稽 <81504466+huaji8@users.noreply.github.com>
2024-05-21 23:44:54 +0800

    新增活跃任务

--- 脉冲网.py
+++ 脉冲网.py
@@ -2,7 +2,7 @@
 #   入口http://mcwreg.bfyxh.cn/h5#/pages/regindex/index?uid=508
 #   抓任意接口 token参数的值
 #   变量:yuanshen_mcw 多号： @分割
-#   听说是 一个金币 1 块钱 0 手续费
+#   一个金币 1 块钱 0 手续费
 #   --------------------------------一般不动区-------------------------------
 #                     _ooOoo_
 #                    o8888888o
@@ -74,19 +74,76 @@ class yuanshen():
         while True:
             id += 1
             if id in id_list:
-                print(f"第[{id}]极奖励已领取")
+                print(f"第[{id}]奖励已领取")
                 continue
             data = {"id":id}
             r = requests.post(url,headers=self.h,json=data).json()
             if r['code'] == 0:
-                print(f"第[{id}]极奖励领取成功")
+                print(f"第[{id}]奖励领取成功")
             else:
-                print(f"第[{id}]极奖励领取失败---[{r['msg']}]")
+                print(f"第[{id}]奖励领取失败---[{r['msg']}]")
                 break
             time.sleep(random.randint(3,10))
     
+    def get_t(self):
+        id_list = [1,5,9]
+        for i in id_list:
+            url = "http://mcw.bfyxh.cn/api/task/lqtask"
+            data = {"id":i}
+            print(data)
+            r = requests.post(url,headers=self.h,json=data).json()
+            if r['code'] == 0:
+                print(f"第[{i}]分润领取成功")
+            else:
+                print(f"第[{i}]分润领取失败---[{r['msg']}]")
+            time.sleep(random.randint(3,10))
+    
+    def do_t(self):
+        # 阅读任务
+        t = 0
+        url = "http://mcw.bfyxh.cn/api/project/getlist"
+        d = {"limit":21,"page":1}
+        r = requests.post(url,headers=self.h,json=d).json()
+        if r['code'] == 0:
+            for i in r['data']['data']:
+                id = i.get("id")
+                d = {"id":f"{id}"}
+                url = "http://mcw.bfyxh.cn/api/project/readproject"
+                r = requests.post(url,headers=self.h,json=d).json()
+                if r['code'] == 0:
+                    print(f"[{id}]阅读成功")
+                    t += 1
+                else:
+                    print(f"[{id}]阅读失败---[{r['msg']}]")
+                if t == 10:
+                    break
+                time.sleep(random.randint(8,15))
+        
+        # 点赞任务
+
+        #傻逼平台
+
+        # 领取
+        id_list = [1,5,9]
+        url = "http://mcw.bfyxh.cn/api/task/getactivity"
+        for i in id_list:
+            d = {"id":i}
+            r = requests.post(url,headers=self.h,json=d).json()
+            if r['code'] == 0:
+                print(f"第[{i}]活跃度任务奖励领取成功")
+            else:
+                print(f"第[{i}]活跃度任务奖励领取失败---[{r['msg']}]")
+            time.sleep(random.randint(8,15))
+
+
+
+
+
+    
 
     def main(self):
+        self.get_t()
+        self.do_t()
         self.task()
 
 if __name__ == '__main__':
